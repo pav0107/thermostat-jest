4b96cfaf638138d640a364dec1bf734439fbc9af
Pav <pav0107@gmail.com>
2021-10-19 19:25:28 +0100

    user can set current temperature

--- interface.js
+++ interface.js
@@ -8,6 +8,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const resetEl = document.querySelector('#temperature-reset');
   const powerSavingModeOnEl = document.querySelector('#powersaving-on');
   const powerSavingModeOffEl = document.querySelector('#powersaving-off');
+  const currentTemperatureEl = document.querySelector('#current-temperature');
+  const selectCityEl = document.querySelector('#select-city');
+  const currentCity = document.querySelector('#current-city');
 
   // Update temperature display
   const updateTemperature = () => {
@@ -53,11 +56,21 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Get weather info for London
-  const API = 'f5929c891d8537828818b3a10a7e3e0e';
-  const city = 'London';
-  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},uk&appid=${API}`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  const displayWeather = (city) => {
+    const API = 'f5929c891d8537828818b3a10a7e3e0e';
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}&units=metric`;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => (currentTemperatureEl.innerText = data.main.temp));
+  };
+
+  displayWeather('London');
+
+  selectCityEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const city = currentCity.value;
+    displayWeather(city);
+  });
 });
